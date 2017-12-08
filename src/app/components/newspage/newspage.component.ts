6aa5579f93bf3e324e88f0731e943156d187523f
emir avdispahic <eavdispahi1@etf.unsa.ba>
2017-12-08 17:52:54 +0100

    Vijesti

--- src/app/components/newspage/newspage.component.ts
+++ src/app/components/newspage/newspage.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newspage.component.css']
 })
 export class NewspageComponent implements OnInit {
-
+showStyle: false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  getStyle() {
+    if(this.showStyle) {
+      return "yellow";
+    } else {
+      return "";
+    }
+  }
 }
