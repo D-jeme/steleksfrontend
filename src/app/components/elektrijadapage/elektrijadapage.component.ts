7d84a78cf92da92cd99b4e1361f6b78cd28e1e50
Amila Mujak <you@example.com>
2017-12-11 23:12:20 +0100

    elektrijada i sifi

--- src/app/components/elektrijadapage/elektrijadapage.component.ts
+++ src/app/components/elektrijadapage/elektrijadapage.component.ts
@@ -11,5 +11,17 @@ export class ElektrijadapageComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  f(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 }
