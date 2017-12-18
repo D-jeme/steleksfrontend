a063bcd660d0674f32390bdf3904717691f14766
D-jeme <eavdispahi1@etf.unsa.ba>
2017-12-18 14:39:54 +0100

    puf

--- src/app/components/elektrijadapage/elektrijadapage.component.ts
+++ src/app/components/elektrijadapage/elektrijadapage.component.ts
@@ -1,27 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Elektrijada } from '../../models/elektrijada';
 @Component({
   selector: 'app-elektrijadapage',
   templateUrl: './elektrijadapage.component.html',
   styleUrls: ['./elektrijadapage.component.css']
 })
 export class ElektrijadapageComponent implements OnInit {
+  private godineElektrijade: Array<String>;
+  //private godineElektrijeSadrzaj: Array<Elektrijada>;
+  private godineElektrijadeSadrzaj: Array<String>;
 
-  constructor() { }
+  private trenutniSadrzaj: String;
+  private trenutniTab:number;
+  private brojac:number
 
+  constructor() {
+    this.godineElektrijade = ['2012','2013','2014','2015','2016','2017'];
+    this.godineElektrijadeSadrzaj= ['Elektrijada 2012', 'Elektrijada 2013','Elektrijada 2014', 'Elektrijada 2015', 'Elektrijada 2016', 'Elektrijada 2017'] ;
+
+  }
   ngOnInit() {
   }
-  f(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+
+  f(godina:String):void{
+    for (let i of this.godineElektrijadeSadrzaj) {
+      this.brojac++;
+      if(i==godina)
+      {
+          //this.trenutniTab=i;
+          this.trenutniSadrzaj=i;
+}
+
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+      console.log(this.trenutniSadrzaj);
+      }
+
+
+
 }
---
+++ src/app/models/elektrijada.ts
@@ -0,0 +1,13 @@
+export class Elektrijada {
+
+naziv: String;
+
+sadrzaj: String;
+
+sponzori: Array<String>;
+
+rezultat: Array<String>;
+
+individualniRezultati: Array<String>;
+
+}
