342667fd29acf50405af2360936cec56623343da
unknown <aaljicevic1@etf.unsa.ba>
2018-01-11 18:18:26 +0100

    dodana funkcija za klik na godine na elektrijadapage.component.ts i izmijenjen dio iznad sponzora na početnoj strani

--- src/app/components/elektrijadapage/elektrijadapage.component.ts
+++ src/app/components/elektrijadapage/elektrijadapage.component.ts
@@ -11,27 +11,35 @@ export class ElektrijadapageComponent implements OnInit {
   private godineElektrijadeSadrzaj: Array<String>;
 
   private trenutniSadrzaj: String;
+  private sadrzaj: String;
   private trenutniTab:number;
-  private brojac:number
+  private brojac:number=0;
+  private godina:String="";
 
   constructor() {
     this.godineElektrijade = ['2012','2013','2014','2015','2016','2017'];
-    this.godineElektrijadeSadrzaj= ['Elektrijada 2012', 'Elektrijada 2013','Elektrijada 2014', 'Elektrijada 2015', 'Elektrijada 2016', 'Elektrijada 2017'] ;
+    this.godineElektrijadeSadrzaj= ['Medunarodni susreti studenata elektrotehnike, poznatiji kao Elektrijada, tradicionalno se odravaju skoro pola stoljeca na prostorima bive Jugoslavije i predstavljaju najveci godinji skup studenata elektrotehnike u jugoistocnoj Evropi, a i ire. Nakon perioda kriznih godina i ratnih deavanja, Elektrijada kao simbol jedinstva globalne akademske zajednice i studentskog duha, ipak opstaje i svojim djelovanjem doprinosi cilju cvrceg povezivanja, saradnje i razmjene studenata elektrotehnike. Elektrijada kroz svoje programe iri i popularie sve sfere aktivnog studentskog ivota  naucni rad, takmicenja u znanju, sportska takmicenja, edukativne programe, zabavu... Ali ono to je najbitnije napomenuti jeste cinjenica da Elektrijada, kao veliki susret studenata ovog tipa, predstavlja multietnicki i multinacionalni projekat vrijedan panje. Petodnevni program Elektrijade se odrava na atraktivnim turistickim destinacijama, gdje se uz dobro organizovan program, lijepu prirodu i dobro poznatu energiju studenata, navedene aktivnosti sprovode u atmosferi zdrave konkurencije i toplog prijateljstva. Putem Elektrijade studenti stupaju u kontakt sa privredom i firmama iz struke, uz ciju pomoc dolaze do sredstava za organizaciju skupa i na taj nacin ostvaruju prve korake poslovne saradnje korisne za buducnost. Posljednjih godina Elektrijada broji preko 2000 ucesnika sa 30 fakulteta iz citave Evrope.', 'Elektrijada 2013','Elektrijada 2014', 'Elektrijada 2015', 'Elektrijada 2016', 'Elektrijada 2017'] ;
 
   }
   ngOnInit() {
   }
 
   f(godina:String):void{
-    for (let i of this.godineElektrijadeSadrzaj) {
+    this.brojac=0;
+    console.log("Pozvana");
+    for (let i of this.godineElektrijade) {
       this.brojac++;
+    //  console.log(i);
       if(i==godina)
       {
           //this.trenutniTab=i;
-          this.trenutniSadrzaj=i;
+          this.godina=i;
+          break;
 }
-
     }
-      console.log(this.trenutniSadrzaj);
+
+this.sadrzaj=this.godineElektrijadeSadrzaj[this.brojac-1];
+
       }
+
 }
