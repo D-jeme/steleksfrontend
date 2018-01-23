f570cd629a50a2c818fdf98735e6193aee485974
D-jeme <eavdispahi1@etf.unsa.ba>
2018-01-23 16:53:54 +0100

    prije skype-a

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -8,13 +8,26 @@ import {PageScrollConfig} from 'ng2-page-scroll';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  host: {
+       '(window:scroll)': 'updateHeader($event)'
+   }
 })
 export class NavigationComponent implements OnInit {
+  isScrolled=false;
+  currPos:Number =0;
+  startPos:Number =0;
+  changePos: Number=100;
+  constructor(){}
 
-  constructor() {
-
-   }
+  updateHeader(evt) {
+      this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+      if(this.currPos >= this.changePos ) {
+          this.isScrolled = true;
+      } else {
+          this.isScrolled = false;
+      }
+  }
 
   ngOnInit() {
   }
