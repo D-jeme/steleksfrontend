444068bf515620acc0e7f9056a5777b2dfda5340
Amila Mujak <you@example.com>
2018-02-13 14:07:53 +0100

    admin, registracija i prijava

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -7,13 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
   private moderatori: Array<String>;
-  private brisanje: Array<String>;
+  private br: number;
+  private showSelected: boolean;
+  private ime: string= '';
+  private prezime: string= '';
+  private messageUspjesno: string= '';
+  private errorMessageIme: string= '';
+  private errorMessagePrezime: string= '';
   constructor() {
-    this.moderatori = ['Prvi moderator','Drugi moderator','Treci moderator','Cetvrti moderator','Peti moderator'];
-    this.brisanje= ['Brisi', 'Brisi', 'Brisi', 'Brisi', 'Brisi']
+    this.br=0;
+    this.showSelected=false;
+    this.moderatori = ['Mujak Amila','Drugi moderator','Treci moderator','Cetvrti moderator','Peti moderator'];
   }
 
   ngOnInit() {
   }
 
+  redniBroj(i) {
+    this.br=0;
+    for(let n in this.moderatori) {
+      this.br++;
+      if(this.moderatori[n]==i) {
+        return this.br;
+      }
+    }
+    return this.br;
+  }
+
+  obrisiModeratora(n) {
+      this.moderatori.splice(n, 1);
+      this.br--;
+  }
+
+  ShowButton(){
+    this.showSelected = true;
+  }
+
+  HideButton(){
+    this.messageUspjesno='';
+    this.errorMessageIme='';
+    this.errorMessagePrezime='';
+    this.ime='';
+    this.prezime='';
+    this.showSelected = false;
+  }
+
+  dodajModeratora() {
+    if(this.ime=='') {
+      this.errorMessagePrezime='';
+      this.errorMessageIme='Molimo Vas da unesete ime!';
+    }
+    else if(this.prezime=='') {
+      this.errorMessageIme='';
+      this.errorMessagePrezime='Molimo Vas da unesete prezime!';
+    }
+    else {
+      this.errorMessageIme='';
+      this.errorMessagePrezime='';
+      this.br++;
+      this.moderatori.push(this.ime+' '+this.prezime);
+      this.messageUspjesno='Uspjesno ste dodali novog moderatora.';
+    }
+  }
 }
