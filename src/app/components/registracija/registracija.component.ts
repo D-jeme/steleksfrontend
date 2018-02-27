aa4790d646893a4b5054a49ce2209203f2cf2e8b
Amila Mujak <you@example.com>
2018-02-27 20:21:15 +0100

    validacija za registraciju

--- src/app/components/registracija/registracija.component.ts
+++ src/app/components/registracija/registracija.component.ts
@@ -1,13 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RegistrovaniClan } from '../../models/registracijastudent';
 @Component({
   selector: 'app-registracija',
   templateUrl: './registracija.component.html',
   styleUrls: ['./registracija.component.css']
 })
 export class RegistracijaComponent implements OnInit {
+  private registrovani: Array<RegistrovaniClan>;
+  private ime: String='';
+  private prezime: String='';
+  private email: String='';
+  private emailFakultet: String='';
+  private brojTelefona: String='';
+  private brojIndeksa: String='';
+  private korisnickoIme: String='';
+  private lozinka: String='';
+  private lozinka2: String='';
+  private odsjek: String='';
+  private godinaStudija: String='';
+  private errorMessage: String='';
+  private messageUspjesno: String='';
+  private novi: RegistrovaniClan;
+
+  constructor() {
+    this.registrovani=new Array<RegistrovaniClan>();
+  }
 
   ngOnInit() {
-
   }
+
+  dodajClan() {
+    this.errorMessage='';
+    console.log("imeeee");
+    console.log(this.ime);
+    if(this.ime=='' || this.prezime=='' || this.email=='' || this.emailFakultet=='' || this.brojTelefona=='' ||
+    this.brojIndeksa=='' || this.korisnickoIme=='' || this.lozinka=='' || this.lozinka2=='') {
+      this.errorMessage='Molimo popunite sva polja!';
+      this.messageUspjesno='';
+      return;
+    }
+    else if(this.lozinka!=this.lozinka2) {
+      this.errorMessage='Lozinke se ne podudaraju!';
+      this.messageUspjesno='';
+      return;
+    }
+    console.log("ima li te");
+    this.novi=new RegistrovaniClan(this.ime, this.prezime, this.email, this.emailFakultet, this.brojTelefona, this.brojIndeksa,
+    this.korisnickoIme, this.lozinka, this.odsjek, this.godinaStudija);
+    this.registrovani.push(this.novi);
+    this.errorMessage='';
+    this.messageUspjesno='Uspjesno!';
+    }
 }
---
+++ src/app/models/registracijastudent.ts
@@ -0,0 +1,25 @@
+export class RegistrovaniClan {
+    ime: String;
+    prezime: String;
+    email: String;
+    emailFakultet: String;
+    brojTelefona: String;
+    brojIndeksa: String;
+    korisnickoIme: String;
+    lozinka: String;
+    odsjek: String;
+    godinaStudija: String;
+    constructor(ime: String, prezime: String, email: String, emailFakultet: String, brojTelefona: String, brojIndeksa: String,
+    korisnickoIme: String, lozinka: String, odsjek: String, godinaStudija: String) {
+        this.ime=ime;
+        this.prezime=prezime;
+        this.email=email;
+        this.emailFakultet=emailFakultet;
+        this.brojTelefona=brojTelefona;
+        this.brojIndeksa=brojIndeksa;
+        this.korisnickoIme=korisnickoIme;
+        this.lozinka=lozinka;
+        this.odsjek=odsjek;
+        this.godinaStudija=godinaStudija;
+    }
+    }
