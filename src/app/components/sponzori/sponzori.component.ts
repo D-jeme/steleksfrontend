8d1ca5e2e0ad800a737315994a17e87848d6857d
Amila Mujak <you@example.com>
2018-02-28 17:18:35 +0100

    sponzori niz i model

--- src/app/components/sponzori/sponzori.component.ts
+++ src/app/components/sponzori/sponzori.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sponzor } from '../../models/sponzori';
 
 @Component({
   selector: 'app-sponzori',
@@ -6,8 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sponzori.component.css']
 })
 export class SponzoriComponent implements OnInit {
-
-  constructor() { }
+  private bhtelecom: String;
+  private bsts: String;
+  private disti: String;
+  private diveco: String;
+  private infostudio: String;
+  private mistral: String;
+  private zira: String;
+  private sue: String;
+  private sponzori: Array<Sponzor>;
+  constructor() {
+    this.bhtelecom ='assets/images/bhtelecom2.jpg';
+    this.bsts='assets/images/BSTS_logo2.png';
+    this.disti='assets/images/disti2.jpg';
+    this.diveco='assets/images/diveco2.png';
+    this.infostudio='assets/images/infostudio2.png';
+    this.mistral='assets/images/mistral.png';
+    this.zira='assets/images/zira2.png';
+    this.sue='assets/images/sue2.jpg';
+    this.sponzori=new Array<Sponzor>();
+    this.sponzori.push(new Sponzor(this.bhtelecom, 'BH Telecom'));
+    this.sponzori.push(new Sponzor(this.bsts, 'BS Telecom Solutions'));
+    this.sponzori.push(new Sponzor(this.disti, 'Disti'));
+    this.sponzori.push(new Sponzor(this.diveco, 'Diveco Energo d.o.o.'));
+    this.sponzori.push(new Sponzor(this.infostudio, 'InfoStudio'));
+    this.sponzori.push(new Sponzor(this.mistral, 'Mistral'));
+    this.sponzori.push(new Sponzor(this.zira, 'Zira'));
+    this.sponzori.push(new Sponzor(this.sue, 'Energoinvest SUE'));
+  }
 
   ngOnInit() {
   }
---
+++ src/app/models/sponzori.ts
@@ -0,0 +1,8 @@
+export class Sponzor {
+  private slika: String;
+  private nazivSponzora: String;
+  constructor(slika: String, nazivSponzora: String) {
+    this.slika=slika;
+    this.nazivSponzora=nazivSponzora;
+  }
+}
