3c06f802aabf0b93fef1f64da019c36e2b1678cf
unknown <you@example.com>
2018-03-01 12:29:13 +0100

    formaelektrijada,model elektrijada

--- src/app/components/formaelektrijada/formaelektrijada.component.ts
+++ src/app/components/formaelektrijada/formaelektrijada.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Elektrijada } from '../../models/elektrijada';
+import { Medalje } from '../../models/medalje';
 
 @Component({
   selector: 'app-formaelektrijada',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formaelektrijada.component.css']
 })
 export class FormaElektrijadaComponent implements OnInit {
+  elektrijada: Elektrijada;
+  fakulteti:Array<String>;
+  medalje:Medalje;
+  naziv:String='';
+  godina:String='';
+  sadrzaj:String='';
+  faculty1:String='';
+  faculty2:String='';
+  faculty3:String='';
 
-  constructor() { }
+
+  constructor() {
+  this.fakulteti=new Array<String>();
+  this.fakulteti=[this.faculty1,this.faculty2,this.faculty3];
+  this.medalje=new Medalje(null,null,this.fakulteti,null,null);
+  this.elektrijada=new Elektrijada(this.naziv,this.godina,this.sadrzaj,this.medalje,null);
+
+  }
 
   ngOnInit() {
   }
 
+  /*print(){
+    console.log(this.Username+":"+this.Password);
+    this._prijavaService.prijava(this.Username, this.Password);
+    this.router.navigateByUrl('/adminopcije');
+  }*/
 }
