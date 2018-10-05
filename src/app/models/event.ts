df42542728b6ccd7830c0211dec3af37a9649e63
unknown <you@example.com>
2018-10-05 19:02:53 +0200

    popravljen prikaz vijesti

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -33,6 +33,15 @@ constructor(id: String,naziv: String,slika: String,kratkiTekst: String, tekst:St
   this.dan=dan;
   this.tipEventa=tipEventa;
 }
+/*constructor(id: String,naziv: String, slika: String, kratkiTekst: String,  mjesec: String, dan: String, tipEventa: String) {
+    this.id=id;
+    this.naziv=naziv;
+    this.kratkiTekst=kratkiTekst;
+    this.slika=slika;
+    this.mjesec=mjesec;
+    this.dan=dan;
+    this.tipEventa=tipEventa;
+}*/
 
 
 }
