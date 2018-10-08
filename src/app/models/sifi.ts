2517a6ef24339b46c7d7ad457d143af849415bd9
Amila <amujak1@etf.unsa.ba>
2018-10-08 18:21:20 +0200

    zavrsen sifi

--- src/app/models/sifi.ts
+++ src/app/models/sifi.ts
@@ -1,13 +1,38 @@
 export class Sifi {
 
-naziv: String;
+godina: String;
 
 sadrzaj: String;
 
-sponzori: Array<String>;
+naziv1: String;
 
-rezultat: Array<String>;
+naziv2: String;
 
-individualniRezultati: Array<String>;
+naziv3: String;
+
+rad: String;
+
+ucesnici1: Array<String>;
+
+ucesnici2: Array<String>;
+
+ucesnici3: Array<String>;
+
+constructor(naziv1: String, naziv2: String, naziv3: String, godina: String,sadrzaj: String,ucesnici1: Array<String>, ucesnici2: Array<String>,
+ucesnici3: Array<String>) {
+  this.naziv1=naziv1;
+  this.naziv2=naziv2;
+  this.naziv3=naziv3;
+  this.godina=godina;
+  this.sadrzaj=sadrzaj;
+  this.ucesnici1=ucesnici1;
+  this.ucesnici2=ucesnici2;
+  this.ucesnici3=ucesnici3;
+  //this.rad=rad;
+}
+
+dajSadrzaj() {
+  return this.sadrzaj;
+}
 
 }
